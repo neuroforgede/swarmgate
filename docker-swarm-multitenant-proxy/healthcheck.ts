d3a42cb729c4011d46921df5c52fe8c0336905ba
Martin Braun <braun@neuroforge.de>
2023-11-12 16:29:54 +0100

    fix healthchecks for https

--- docker-swarm-multitenant-proxy/healthcheck.ts
+++ docker-swarm-multitenant-proxy/healthcheck.ts
@@ -1,22 +1,46 @@
-// from https://github.com/BretFisher/node-docker-good-defaults
+// adapted from https://github.com/BretFisher/node-docker-good-defaults
 import http from 'http';
+import https from 'https';
 
-var options = {
-  timeout: 2000,
-  host: 'localhost',
-  port: process.env.PORT || 8080,
-  path: '/_healthz'
-};
+const TLS_DISABLED = process.env.TLS_DISABLED === '1' || process.env.TLS_DISABLED === 'true';
 
-var request = http.request(options, (res) => {
-  console.info('STATUS: ' + res.statusCode);
-  process.exitCode = (res.statusCode === 200) ? 0 : 1;
-  process.exit();
-});
+if (!TLS_DISABLED) {
+  (process.env as any)["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+  var options = {
+    timeout: 2000,
+    host: 'localhost',
+    port: process.env.PORT || 8080,
+    path: '/_healthz'
+  };
 
-request.on('error', function (err) {
-  console.error('ERROR', err);
-  process.exit(1);
-});
+  var request = https.request(options, (res) => {
+    console.info('STATUS: ' + res.statusCode);
+    process.exitCode = (res.statusCode === 200) ? 0 : 1;
+    process.exit();
+  });
 
-request.end();
+  request.on('error', function (err) {
+    console.error('ERROR', err);
+    process.exit(1);
+  });
+
+  request.end();
+} else {
+  var options = {
+    timeout: 2000,
+    host: 'localhost',
+    port: process.env.PORT || 8080,
+    path: '/_healthz'
+  };
+
+  var request = http.request(options, (res) => {
+    console.info('STATUS: ' + res.statusCode);
+    process.exitCode = (res.statusCode === 200) ? 0 : 1;
+    process.exit();
+  });
+
+  request.on('error', function (err) {
+    console.error('ERROR', err);
+    process.exit(1);
+  });
+}
