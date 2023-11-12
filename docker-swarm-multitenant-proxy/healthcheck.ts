d23952940658d1aa9361bc00ef664c52095c4c41
Martin Braun <braun@neuroforge.de>
2023-11-12 16:40:39 +0100

    use ssl cert of server for healthchecks

--- docker-swarm-multitenant-proxy/healthcheck.ts
+++ docker-swarm-multitenant-proxy/healthcheck.ts
@@ -1,16 +1,24 @@
 // adapted from https://github.com/BretFisher/node-docker-good-defaults
 import http from 'http';
 import https from 'https';
+import fs from 'fs';
 
 const TLS_DISABLED = process.env.TLS_DISABLED === '1' || process.env.TLS_DISABLED === 'true';
 
 if (!TLS_DISABLED) {
-  (process.env as any)["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-  var options = {
+  if(!process.env.TLS_KEY_FILE || !process.env.TLS_CERT_FILE || !process.env.TLS_CA_FILE) {
+    console.error('ERROR: TLS is enabled but one or more of the following environment variables are not set: TLS_KEY_FILE, TLS_CERT_FILE, TLS_CA_FILE');
+    process.exit(1);
+  }
+
+  let options = {
     timeout: 2000,
     host: 'localhost',
     port: process.env.PORT || 8080,
-    path: '/_healthz'
+    path: '/_healthz',
+    key: fs.readFileSync(process.env.TLS_KEY_FILE),
+    cert: fs.readFileSync(process.env.TLS_CERT_FILE),
+    ca: fs.readFileSync(process.env.TLS_CA_FILE),
   };
 
   var request = https.request(options, (res) => {
@@ -26,7 +34,7 @@ if (!TLS_DISABLED) {
 
   request.end();
 } else {
-  var options = {
+  let options = {
     timeout: 2000,
     host: 'localhost',
     port: process.env.PORT || 8080,
